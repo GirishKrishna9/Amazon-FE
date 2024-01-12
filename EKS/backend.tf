e4e620f6cfa70d11315bcdb0e82e9fd6e3189696
GirishKrishna9 <147157491+GirishKrishna9@users.noreply.github.com>
2024-01-12 11:35:56 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "infra-terraform-bucket-v1"  # Replace with your actual S3 bucket name
+    bucket = "giriproject"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-south-1" 
   }
